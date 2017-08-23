a91035efad92f2b26118fcd15c1e27b58e8c3087
Kuikka Maria <maria.kuikka@almamedia.fi>
2017-08-23 12:40:01 +0300

    data fetched from json file

--- analysestock/src/components/CompanyData.js
+++ analysestock/src/components/CompanyData.js
@@ -1,27 +1,35 @@
 import React, { Component } from "react";
+import nokiaData from "../nokia.json";
 
 class CompanyData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: nokiaData.companyDetailsList[0].values
     };
   }
 
   componentDidMount() {
-    fetch(
+    /* fetch(
       `http://tomcat-8700-test-1.kauppalehti.biz:8700/integration-companydata-1.16-SNAPSHOT/companydetails/json/1120390`
     )
       .then(result => result.json())
-      .then(data => this.setState({ data: data.companyDetailsList.values }));
+      .then(data => this.setState({ data: data.companyDetailsList.values }));*/
   }
 
   render() {
+    console.log(nokiaData);
     if (!this.state.data) {
       return <div>...loading... </div>;
     }
     return (
       <ul>
+        <li>
+          <span>Nimi:</span>
+          <span>
+            {this.state.data.NIMI}
+          </span>
+        </li>
         <li>
           <span>Nimi:</span>
           <span>
